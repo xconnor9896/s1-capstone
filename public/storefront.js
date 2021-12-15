4ae495d7a45db333af96aa1613167c067d131118
Connor Kobriger <cjkobriger@gmail.com>
2021-12-15 13:15:38 -0700

    pushing AGAIN! UGH

--- public/storefront.js
+++ public/storefront.js
@@ -48,21 +48,21 @@ imageInput.addEventListener('change', async (e) => {
   }
 })
 
-// async function fetchProducts() {
-//   try {
-//     const {
-//       data: { products }
-//     } = await axios.get(url);
-//     const tempProducts = products.map(each => {
-//       return `<article class="product">
-//             <img src="${each.image}" alt="${each.name}" class"img"/>
-//             <footer><p>${each.name}</p>
-//             <span>${each.price}</span></footer></article>`
-//     }).join("")
-//     container.innerHTML = tempProducts;
-//   } catch (err) {
+async function fetchProducts() {
+  try {
+    const {
+      data: { products }
+    } = await axios.get(url);
+    const tempProducts = products.map(each => {
+      return `<article class="product">
+            <img src="${each.image}" alt="${each.name}" class"img"/>
+            <footer><p>${each.name}</p>
+            <span>${each.price}</span></footer></article>`
+    }).join("")
+    container.innerHTML = tempProducts;
+  } catch (err) {
 
-//   }
-// }
+  }
+}
 
-// fetchProducts();
+fetchProducts();
